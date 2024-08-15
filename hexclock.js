4e775b6831b3d91aceb52d5f24f41d2da9fb68ef
Adam <adam@thinkaliker.com>
2024-08-14 22:18:09 -0700

    additional cleanup, add comments

--- hexclock.js
+++ hexclock.js
@@ -2,61 +2,54 @@ let mode = "scaled";
 
 $(document).ready(function() {
 
+  // time to color calculator
   setInterval(function() {
-
     const d = new Date();
     const h = d.getHours();
     const m = d.getMinutes();
     const s = d.getSeconds();
-    //h = 12; m = 12; s = 59;
+    //h = 13; m = 30; s = 30;
 
-    const hScaled = mapRange(h, 0, 23, 0, 230).toString(16);
-    const mScaled = mapRange(m, 0, 59, 0, 236).toString(16);
-    const sScaled = mapRange(s, 0, 59, 0, 236).toString(16);
+    let bgColor = null;
+    let clockColor = null;
 
-    const colorNormal = rgbToHex(h, m, s);
-    const colorScaled = rgbToHex(hScaled, mScaled, sScaled);
-
-    let textCol = "";
     if (mode === "scaled") {
-      textCol = textColorFromBackground(hScaled, mScaled, sScaled);
-    } else {
-      textCol = textColorFromBackground(h, m, s);
+      const hScaled = mapRange(h, 0, 23, 0, 230).toString(16);
+      const mScaled = mapRange(m, 0, 59, 0, 236).toString(16);
+      const sScaled = mapRange(s, 0, 59, 0, 236).toString(16);
+      bgColor = rgbToHexString(hScaled, mScaled, sScaled);
+      clockColor = textColorFromBackground(hScaled, mScaled, sScaled);
+    } else if (mode === "normal") {
+      bgColor = rgbToHexString(h, m, s);
+      clockColor = textColorFromBackground(h, m, s);
     }
-     
-    $(".scaled").css("background-color", colorScaled);
-    $(".scaledtext").text(colorScaled);
-    $(".normal").css("background-color", colorNormal);
-    $(".normaltext").text(colorNormal);
-    $(".scaledtext").css("color", textCol);
+
+    $("body").css("background-color", bgColor);
+    $("#clock").text(String(bgColor));
+    $("#clock").css("color", clockColor);
   }, 100);
 
+  // mode switcher
   $("#clock").click(function() {
-    if(mode ===  "scaled"){
-      $(".scaled").addClass("normal");
-      $(".normal").removeClass("scaled");
-      $("#clock").addClass("normaltext");
-      $("#clock").removeClass("scaledtext");
+    if (mode ===  "scaled"){
       mode = "normal";
-    } else {
-      $(".normal").addClass("scaled");
-      $(".scaled").removeClass("normal");
-      $("#clock").addClass("scaledtext");
-      $("#clock").removeClass("normaltext");
+    } else if (mode === "normal"){
       mode = "scaled";
     }
   });
 });
 
-function rgbToHex(r, g, b) {
-  return (`#${("00" + r).substr(-2,2)}${("00" + g).substr(-2,2)}${("00" + b).substr(-2,2)}`);
+// converts 3 rgb (or other set of 3 numbers) values into a hex string
+function rgbToHexString(r, g, b) {
+  return (`#${String(r).padStart(2, '0')}${String(g).padStart(2, '0')}${String(b).padStart(2, '0')}`);
 }
 
+// maps a value from one range to another range
 function mapRange(value, low1, high1, low2, high2) {
   return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
 
-
+// choose a text color based on the provided RGB values
 function textColorFromBackground(r, g, b) {
   //console.log(parseInt(r, 16) * 0.299, parseInt(g, 16) * 0.586, parseInt(b, 16) * 0.114)
   return (((parseInt(r, 16) * 0.299) + (parseInt(g, 16) * 0.587) + (parseInt(b, 16) * 0.114)) > 186) ?
