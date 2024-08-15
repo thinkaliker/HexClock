8127d03a459b7cc7e78f0033f8948a168ec81216
Adam <adam@thinkaliker.com>
2024-08-14 21:25:04 -0700

    switch to jetbrains mono, add contrast text, fix flexbox, cleanup

--- hexclock.js
+++ hexclock.js
@@ -1,53 +1,64 @@
-var toggle = false;
+let mode = "scaled";
 
 $(document).ready(function() {
 
   setInterval(function() {
 
-    var d = new Date();
-    var h = d.getHours();
-    var m = d.getMinutes();
-    var s = d.getSeconds();
+    const d = new Date();
+    const h = d.getHours();
+    const m = d.getMinutes();
+    const s = d.getSeconds();
+    //h = 12; m = 12; s = 59;
 
-    var color_n = normal(h, m, s);
-    var color_s = scaled(h, m, s);
+    const hScaled = mapRange(h, 0, 23, 0, 230).toString(16);
+    const mScaled = mapRange(m, 0, 59, 0, 236).toString(16);
+    const sScaled = mapRange(s, 0, 59, 0, 236).toString(16);
 
-    $(".scaled").css("background-color", color_s);
-    $(".scaledtext").text(color_s);
-    $(".normal").css("background-color", color_n);
-    $(".normaltext").text(color_n);
+    const colorNormal = rgbToHex(h, m, s);
+    const colorScaled = rgbToHex(hScaled, mScaled, sScaled);
+
+    let textCol = "";
+    if (mode === "scaled") {
+      textCol = textColorFromBackground(hScaled, mScaled, sScaled);
+    } else {
+      textCol = textColorFromBackground(h, m, s);
+    }
+     
+    $(".scaled").css("background-color", colorScaled);
+    $(".scaledtext").text(colorScaled);
+    $(".normal").css("background-color", colorNormal);
+    $(".normaltext").text(colorNormal);
+    $(".scaledtext").css("color", textCol);
   }, 100);
 
   $("#clock").click(function() {
-    if(toggle){
+    if(mode ===  "scaled"){
       $(".scaled").addClass("normal");
       $(".normal").removeClass("scaled");
       $("#clock").addClass("normaltext");
       $("#clock").removeClass("scaledtext");
-      toggle = false;
+      mode = "normal";
     } else {
       $(".normal").addClass("scaled");
       $(".scaled").removeClass("normal");
       $("#clock").addClass("scaledtext");
       $("#clock").removeClass("normaltext");
-      toggle = true;
+      mode = "scaled";
     }
   });
 });
 
-function scaled(h, m, s) {
-  var h_s = map_range(h, 0, 23, 0, 230).toString(16);
-  var m_s = map_range(m, 0, 59, 0, 236).toString(16);
-  var s_s = map_range(s, 0, 59, 0, 236).toString(16);
-  //console.info("" + h_s +" "+ m_s+" "+ s_s + " ");
-  return ("#" + ("00" + h_s).substr(-2,2) + ("00" + m_s).substr(-2,2) + ("00" + s_s).substr(-2,2));
+function rgbToHex(r, g, b) {
+  return (`#${("00" + r).substr(-2,2)}${("00" + g).substr(-2,2)}${("00" + b).substr(-2,2)}`);
 }
 
-function normal(h, m, s) {
-  //console.info("#" + hz + h + mz + m + sz + s);
-  return ("#" + ("00" + h).substr(-2,2) + ("00" + m).substr(-2,2) + ("00" + s).substr(-2,2));
-}
-
-function map_range(value, low1, high1, low2, high2) {
+function mapRange(value, low1, high1, low2, high2) {
   return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+
+function textColorFromBackground(r, g, b) {
+  //console.log(parseInt(r, 16) * 0.299, parseInt(g, 16) * 0.586, parseInt(b, 16) * 0.114)
+  return (((parseInt(r, 16) * 0.299) + (parseInt(g, 16) * 0.587) + (parseInt(b, 16) * 0.114)) > 186) ?
+  "#000000" : "#ffffff";
 }
